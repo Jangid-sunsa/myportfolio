1a66e8b4270a0e36c3e5c27d1f2317eb81ef2db6
Sunil Jangid <sutharsunil7742@gmail.com>
2025-09-01 17:10:39 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,25 @@
+// JS for hamburger button
 const navbar = document.querySelector(".nav-links");
-// console.log(navbar);
 function onToggleMenu(e) {
   e.name = e.name === "menu" ? "close" : "menu";
-  navbar.classList.toggle("top-[9%]");
+  navbar.classList.toggle("top-[7%]");
 }
 
+// Save data into localstorage using js
+function saveData(event) {
+  event.preventDefault();
 
+  const formData = {
+    uname: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    mNum: document.getElementById("num").value,
+    message: document.getElementById("mssg").value,
+  };
+
+  const saveData = JSON.parse(localStorage.getItem("saveTols")) || [];
+  saveData.push(formData);
+  localStorage.setItem("saveTols", JSON.stringify(saveData));
+
+  document.getElementById("cForm").reset();
+}
 
